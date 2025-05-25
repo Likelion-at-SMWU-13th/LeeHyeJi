e4434646ce73f1bd80d15b8afd0b190ac2dd6137
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-05-25 15:23:43 +0900

    Feat: UserModelViewSet 구현

--- django/week04_hw/users/views.py
+++ django/week04_hw/users/views.py
@@ -7,4 +7,4 @@ from .serializers import UserModelSerializer
 
 class UserModelViewSet(ModelViewSet):
     queryset=User.objects.all()
-    serializer_class=UserModelSerializer
+    serializer_class=UserModelSerializer 
