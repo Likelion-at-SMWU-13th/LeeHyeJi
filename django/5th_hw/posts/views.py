2e877d0c045a453bce7420fae22e72c62e9b0f06
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-06-29 00:33:11 +0900

    feat: PostModelViewSet 구현

--- django/5th_hw/posts/views.py
+++ django/5th_hw/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet 
 
+from .models import Post
+from .serializers import PostModelSerializer
 # Create your views here.
+
+class PostListView(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostModelSerializer
