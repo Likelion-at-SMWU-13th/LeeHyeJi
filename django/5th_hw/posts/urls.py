c0f6d166535af40bfb467ced2ddcc67828d71d1a
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-06-29 16:52:06 +0900

    feat: 게시글 조회, 생성, 수정, 삭제 & 댓글 생성, 조회 구현

--- django/5th_hw/posts/urls.py
+++ django/5th_hw/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
 
-from .views import PostModelViewSet
+from .views import PostModelViewSet,PostListView,PostRetrieveView, CommentCreateView
 
 app_name="posts"
 
@@ -10,6 +10,8 @@ router_post = routers.DefaultRouter()
 router_post.register('', PostModelViewSet)
 
 urlpatterns = [
+    path('comments/', CommentCreateView.as_view()),
     path('', include(router_post.urls)),
-    path('admin/', admin.site.urls)
+    path('',PostListView.as_view()),
+    path('<int:pk>', PostRetrieveView.as_view()),
 ]
--- django/5th_hw/posts/views.py
+++ django/5th_hw/posts/views.py
@@ -1,10 +1,54 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet 
 
-from .models import Post
-from .serializers import PostModelSerializer
+from .models import Post, Comment
+from .serializers import PostModelSerializer,PostListSerializer, PostRetrieveSerializer, CommentListModelSerializer, CommentCreateSerializer
+from rest_framework import generics
+from rest_framework.decorators import action, api_view
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 # Create your views here.
 
-class PostModelViewSet(ModelViewSet):
+#게시글 목록 + 생성
+class PostListView(generics.ListAPIView, generics.CreateAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostModelSerializer
+    serializer_class = PostListSerializer
+
+#게시글 상세 + 수정 + 삭제 
+class PostRetrieveView(generics.RetrieveAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostRetrieveSerializer
+
+#Viewset
+class PostModelViewSet(ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostListSerializer
+
+    @action(detail = True, methods =['GET'])
+    def get_comment_all(self, request, pk=None):
+        post = self.get_object()
+        comment_all = post.comment_set.all()
+        serializer = CommentListModelSerializer(comment_all, many = True)
+        return Response(serializer.data)
+    
+@api_view(['POST'])
+def login_view(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request,
+                        username = username,
+                        password = password)
+    
+    if user : 
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    
+    else:
+        return Response(status=401)
+
+class CommentCreateView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentCreateSerializer
+    permission_classes = [IsAuthenticated]
