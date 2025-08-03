d5e317eb94ad3f7deb917abd141b1b6d17aba1b6
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-03 16:18:50 +0900

    Feat: BookController에 @Controller 어노테이션 추가

--- spring/week03/hw/src/main/java/org/example/hw/controller/BookController.java
+++ spring/week03/hw/src/main/java/org/example/hw/controller/BookController.java
@@ -2,11 +2,13 @@ package org.example.hw.controller;
 
 import org.example.hw.model.Book;
 import org.example.hw.service.BookService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class BookController {
 
     private final BookService bookService;
