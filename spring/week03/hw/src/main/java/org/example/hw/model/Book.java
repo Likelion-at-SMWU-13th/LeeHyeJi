61cf15e5b9bc593a3eb9714efe9ee47f8dead62e
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-03 14:12:46 +0900

    Feat: moBook del 생성

---
+++ spring/week03/hw/src/main/java/org/example/hw/model/Book.java
@@ -0,0 +1,25 @@
+package org.example.hw.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private int price;
+
+    public Book() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
