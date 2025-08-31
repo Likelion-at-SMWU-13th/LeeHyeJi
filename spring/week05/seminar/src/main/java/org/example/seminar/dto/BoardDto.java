8ad0b0539e19d80283b01c942cb52b7b24773e64
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-31 12:37:50 +0900

    Feat: BoardDto 생성

---
+++ spring/week05/seminar/src/main/java/org/example/seminar/dto/BoardDto.java
@@ -0,0 +1,19 @@
+package org.example.seminar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardDto {
+    private Long id;
+    private String name;
+    private List<PostDto> posts = new ArrayList<>();
+}
