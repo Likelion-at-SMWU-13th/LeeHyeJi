7ecdb29f06a0e0e8d973e1b1a95e167e45b5dcc8
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-07 16:59:55 +0900

    Feat: ProductRepository JPQL 코드 작성

--- spring/week06/seminar/src/main/java/org/example/seminar/product/repository/ProductRepository.java
+++ spring/week06/seminar/src/main/java/org/example/seminar/product/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package org.example.seminar.product.repository;
 
 import org.example.seminar.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
@@ -11,7 +13,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // JPA: 가장 비싼 상품 Top10 조회
     List<Product> findTop10ByOrderByPriceDesc();
 
-
+    // JPQL: 가격이 2,000원 이하이고, 재고가 많은 상품 Top5 조회
+    @Query("SELECT p FROM Product p WHERE p.price <= :price ORDER BY p.stock DESC LIMIT 5")
+    List<Product> findTop5ByPriceAndStock(@Param("price") int price);
 
 }
 
