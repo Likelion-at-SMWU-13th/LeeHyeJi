20d6fbd430778f8e8943664da4d3685267edccd8
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-16 19:33:46 +0900

    Feat: CustomException 구현

---
+++ spring/week08/seminar/src/main/java/org/example/seminar/exception/HttpStatus.java
@@ -0,0 +1,43 @@
+package org.example.seminar.exception;
+
+public enum HttpStatus {
+
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable");
+
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + message;
+    }
+
+    public static HttpStatus valueOf(int statusCode) {
+        for (HttpStatus status : HttpStatus.values()) {
+            if (status.code == statusCode) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid HTTP Status code: " + statusCode);
+    }
+    }
