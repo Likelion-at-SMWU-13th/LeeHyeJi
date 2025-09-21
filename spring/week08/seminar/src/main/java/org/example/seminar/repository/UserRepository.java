1395dce7d78c2814d902d5a65b82955011b4dc56
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-21 16:01:48 +0900

    Feat: UserRepository 구현

---
+++ spring/week08/seminar/src/main/java/org/example/seminar/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.seminar.repository;
+
+import org.example.seminar.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+}
