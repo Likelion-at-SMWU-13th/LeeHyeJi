24b7b997f02933ddde944b57eb6fecf845f9ca02
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-21 16:03:14 +0900

    Feat: MethodArgumentNotValidException 등록

--- spring/week08/seminar/src/main/java/org/example/seminar/exception/CustomExceptionHandler.java
+++ spring/week08/seminar/src/main/java/org/example/seminar/exception/CustomExceptionHandler.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -30,6 +32,23 @@ public class CustomExceptionHandler {
 
         return ResponseEntity
                 .status(e.getHttpStatus().getCode())
+                .body(map);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        LOGGER.error("Advice 내 handleMethodArgumentNotValidException 호출, URI: {}", request.getRequestURI());
+
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        String errorMessage = (fieldError != null) ? fieldError.getDefaultMessage() : "Validation failed";
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", HttpStatus.BAD_REQUEST.getMessage());
+        map.put("code", Integer.toString(HttpStatus.BAD_REQUEST.getCode()));
+        map.put("message", errorMessage);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.getCode())
                 .body(map);
     }
 
