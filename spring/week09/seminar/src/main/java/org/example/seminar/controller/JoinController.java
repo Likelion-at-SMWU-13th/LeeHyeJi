9a97abd2588e226062ed573ac2c88bc358786b68
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-23 19:40:30 +0900

    Feat: Spring Security ì실습

---
+++ spring/week09/seminar/src/main/java/org/example/seminar/controller/JoinController.java
@@ -0,0 +1,29 @@
+package org.example.seminar.controller;
+
+import org.example.seminar.dto.JoinDTO;
+import org.example.seminar.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class JoinController {
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @GetMapping("/join")
+    public String joinP() {
+        return "join";
+    }
+
+    @PostMapping("/joinProc")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
+
+        return "redirect:/login";
+    }
+}
